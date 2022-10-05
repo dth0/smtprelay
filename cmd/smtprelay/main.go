c7d69288f1f562e1af8201fd094731fee26d2c19
Daniel Theodoro <daniel.theodoro@gmail.com>
2022-10-05 13:14:59 +0200

    add dkim

--- cmd/smtprelay/main.go
+++ cmd/smtprelay/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"math/rand"
@@ -13,14 +17,15 @@ import (
 	"time"
 
 	"github.com/chrj/smtpd"
+	"github.com/emersion/go-msgauth/dkim"
 	"github.com/kelseyhightower/envconfig"
 )
 
-// TODO: DKIM
+const DKIMPATH = "/etc/smtprelay/dkim"
+
 type Config struct {
 	Listen       string `default:"0.0.0.0:25"`
-	DkimCertFile string `default:"cert.pem"`
-	DkimKeyFile  string `default:"key.pem"`
+	DkimKey      string `default:"dkim.key"`
 	DkimSelector string `default:"default"`
 	DkimDomain   string `default:"example.com"`
 }
@@ -29,6 +34,7 @@ func init() {
 	rand.Seed(time.Now().Unix())
 }
 
+// TODO: return a list of server ordered by priority
 func findNextHop(domain string) (string, error) {
 	entries, err := net.LookupMX(domain)
 	if err != nil {
@@ -46,31 +52,6 @@ func findNextHop(domain string) (string, error) {
 	return host, nil
 }
 
-func smtpClientHandler(_ smtpd.Peer, env smtpd.Envelope) error {
-	// I know that's weird but I only expect to have one recipient each time
-	domain, err := func(email string) (string, error) {
-		addr := strings.Split(email, "@")
-		if len(addr) != 2 {
-			return "", fmt.Errorf("malformed e-mail address: %s", email)
-		}
-
-		return addr[1], nil
-	}(env.Recipients[0])
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	srv, err := findNextHop(domain)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	log.Printf("Sending email from %s to %s via %s\n", env.Sender, env.Recipients[0], srv)
-	return smtp.SendMail(fmt.Sprintf("%s:25", srv), nil, env.Sender, env.Recipients, env.Data)
-}
-
 func parseConfig() (Config, error) {
 	var c Config
 	err := envconfig.Process("smtp", &c)
@@ -78,7 +59,20 @@ func parseConfig() (Config, error) {
 		return c, err
 	}
 
+	log.Printf("Load config: listen to %s, dkim (%s - %s - %s)", c.Listen, c.DkimDomain, c.DkimSelector, c.DkimKey)
+
 	return c, err
+}
+
+func loadPrivateKey(keyfile string) (crypto.Signer, error) {
+	data, err := os.ReadFile(fmt.Sprintf("%s/%s", DKIMPATH, keyfile))
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(data)
+
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
 }
 
 func main() {
@@ -87,9 +81,58 @@ func main() {
 		log.Fatalf("error parsing config: %s\n", err)
 	}
 
+	dkimEnabled := true
+	privatekey, err := loadPrivateKey(cfg.DkimKey)
+	if err != nil {
+		log.Printf("error load the privatekey %s: %s\n", cfg.DkimKey, err)
+		dkimEnabled = false
+	}
+
 	server := &smtpd.Server{
 		MaxRecipients: 1,
-		Handler:       smtpClientHandler,
+		Handler: func(_ smtpd.Peer, env smtpd.Envelope) error {
+
+			domain, err := func(email string) (string, error) {
+				addr := strings.Split(email, "@")
+				if len(addr) != 2 {
+					return "", fmt.Errorf("malformed e-mail address: %s", email)
+				}
+
+				return addr[1], nil
+			}(env.Recipients[0])
+
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+
+			hop, err := findNextHop(domain)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+
+			log.Printf("Sending email from %s to %s via %s\n", env.Sender, env.Recipients[0], hop)
+			if dkimEnabled {
+				dkimOpts := &dkim.SignOptions{
+					Domain:   cfg.DkimDomain,
+					Selector: cfg.DkimSelector,
+					Signer:   privatekey,
+				}
+
+				var msg bytes.Buffer
+
+				if err := dkim.Sign(&msg, bytes.NewReader(env.Data), dkimOpts); err != nil {
+					log.Println(err)
+					return err
+				}
+
+				return smtp.SendMail(fmt.Sprintf("%s:25", hop), nil, env.Sender, env.Recipients, msg.Bytes())
+			}
+
+			return smtp.SendMail(fmt.Sprintf("%s:25", hop), nil, env.Sender, env.Recipients, env.Data)
+
+		},
 	}
 
 	go func() {
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.19
 
 require github.com/chrj/smtpd v0.3.1
 
-require github.com/kelseyhightower/envconfig v1.4.0
+require (
+	github.com/emersion/go-msgauth v0.6.6
+	github.com/kelseyhightower/envconfig v1.4.0
+)
+
+require golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
